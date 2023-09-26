6e559fd72f3ccaeb383918de3b7def6afb0318fd
tammiekoh1518 <142320679+tammiekoh1518@users.noreply.github.com>
2023-09-26 15:49:59 +0800

    formatted

--- binary_file_parser/parser.py/parser.py
+++ binary_file_parser/parser.py/parser.py
@@ -2,16 +2,13 @@ import struct
 import pandas as pd
 
 def parse_binary_file(file_path):
-    # Define byte lengths and unpacking formats
     byte_lengths = {'recnum': 4, 'latitude': 4, 'longitude': 4, 'numvals': 2, 'SS': 4, 'S1': 4}
     unpack_formats = {'recnum': 'i', 'latitude': 'f', 'longitude': 'f', 'numvals': 'h', 'SS': 'f', 'S1': 'f'}
 
-    # Parse the binary file and store data in lists
     recnums, latitudes, longitudes, numvals_list, SS_vals, S1_vals = [], [], [], [], [], []
     with open(file_path, "rb") as file:
         while True:
             try:
-                # Read and unpack the data field-by-field
                 recnum = struct.unpack(unpack_formats['recnum'], file.read(byte_lengths['recnum']))[0]
                 latitude = struct.unpack(unpack_formats['latitude'], file.read(byte_lengths['latitude']))[0]
                 longitude = struct.unpack(unpack_formats['longitude'], file.read(byte_lengths['longitude']))[0]
@@ -26,8 +23,7 @@ def parse_binary_file(file_path):
             numvals_list.append(numvals)
             SS_vals.append(SS)
             S1_vals.append(S1)
-            file.seek(2, 1)  # Skip 2 bytes
+            file.seek(2, 1)  # skip 2 bytes
 
-    # Convert lists to pandas DataFrame
     df = pd.DataFrame({'recnum': recnums, 'latitude': latitudes, 'longitude': longitudes, 'numvals': numvals_list, 'SS': SS_vals, 'S1': S1_vals})
     return df
